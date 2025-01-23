9ca48b8dbf1ceda8812e7d5bdd0f0b950f9aa212
sudhashree4444 <sudhashree4016@gmail.com>
2025-01-23 23:40:19 +0100

    Cinnamon buns program completed

---
+++ A_0_1_Cinnamon_buns.py
@@ -0,0 +1,15 @@
+#This Python script solves the Cinnamon buns pricing with discount
+#Author: TEJA SUDHASHREE DEVAGUPTAPU
+#Date: 23-01-2025
+# A.0.1: Cinnamon buns
+regular_price = 35.00
+discounted_rate = 60
+number_of_kanekbulle_ordered = int(input("number of kanebulle purchased:"  ))
+
+regular_price =  regular_price * number_of_kanekbulle_ordered
+discount_price = regular_price * (discounted_rate/100)
+total_price = regular_price - discount_price
+
+print(f"regular price: {regular_price:.2f} SEK")
+print(f"discount price: {discount_price:.2f} SEK")
+print(f"total price: {total_price:.2f} SEK")
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Lab-1---Getting-Started---Python-Exercises
-This Python script solves the problems from LAB-1 exercises I and VII.  #Author: TEJA SUDHASHREE DEVAGUPTAPU 
+# Lab-1-Getting-Started-Python-Exercises
+This Python script solves the problems from LAB-1 exercises I and VII.  
+
+Author: TEJA SUDHASHREE DEVAGUPTAPU
+# how to run the code
+I have uploaded the code in Git hub, please clone the repo to your local using external applications like intelJ or Visual sudio and
+Run it by opening the each file for each program.
